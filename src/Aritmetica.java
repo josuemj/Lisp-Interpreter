9486d4a78ab0b5299aba86d8e329c225e60a6c7f
MarchMol <98711734+MarchMol@users.noreply.github.com>
2023-03-14 09:21:21 -0600

    Commit 1, reader

---
+++ src/Aritmetica.java
@@ -0,0 +1,25 @@
+public class Aritmetica {
+    public void Suma(int a, int b){
+
+        System.out.println(a+b);
+    }
+    public void Resta(int a, int b){
+        System.out.println(a-b);
+    }
+    public void Multimplicacion(int a, int b){
+        System.out.println(a*b);
+    }
+    public void Division(int a, int b){
+        System.out.println(a/b);
+    }
+
+    public String[] Pre_to_Post(String exp){
+        return null;
+    }
+
+    public void Postfix_Calc(String exp){
+
+    }
+
+
+}
---
+++ src/Reader.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Reader {
+    public static ArrayList<String> Read(){
+        ArrayList<String> codigo = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/lisp.txt"))) {
+            String line;
+            System.out.println("Codigo original:");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                codigo.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("leido");
+        System.out.println("");
+        return codigo;
+    }
+}
